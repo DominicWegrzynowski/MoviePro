7fde0f5602ab95ffdb43ad29e4e2e2c27bf315e0
Dominic Wegrzynowski <dominicwegrzynowski@gmail.com>
2021-11-22 18:30:59 -0500

    Created MovieController

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MoviePro.Data;
+using MoviePro.Models.Database;
+using MoviePro.Models.Settings;
+using MoviePro.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviePro.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppSettings _appSettings;
+        private readonly ApplicationDbContext _context;
+        private readonly IImageService _imageService;
+        private readonly IRemoteMovieService _tmdbMovieService;
+        private readonly IDataMappingService _tmdbMappingService;
+        public MoviesController(IOptions<AppSettings> appSettings, ApplicationDbContext context, IRemoteMovieService tmdbMovieService)
+        {
+            _appSettings = appSettings.Value;
+            _context = context;
+            _tmdbMovieService = tmdbMovieService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Import()
+        {
+            var movies = await _context.Movie.ToListAsync();
+            return View(movies);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Import(int id)
+        {
+            //if movie exists, warn the user instead of importing it again
+            if (_context.Movie.Any(m => m.MovieId == id))
+            {
+                var localMovie = await _context.Movie.FirstOrDefaultAsync(m => m.MovieId == id);
+                return RedirectToAction("Details", "Movies", new { id = localMovie.Id, local = true});
+            }
+
+            //Step 1: get the raw data from the API
+            var movieDetail = await _tmdbMovieService.MovieDetailAsync(id);
+
+            //Step 2: run the data through a mapping procedure
+            var movie = await _tmdbMappingService.MapMovieDetailAsync(movieDetail);
+
+            //Step 3: Add the new movie
+            _context.Add(movie);
+            await _context.SaveChangesAsync();
+
+            //Step 4: Assign it to the default collection
+            await AddToMovieCollection(movie.Id, _appSettings.MovieProSettings.DefaultCollection.Name);
+
+            return RedirectToAction("Import");
+        }
+
+        private async Task AddToMovieCollection(int movieId, string collectionName)
+        {
+            var collection = await _context.Collection.FirstOrDefaultAsync(c => c.Name == collectionName);
+            _context.Add(
+                new MovieCollection
+                {
+                    CollectionId = collection.Id,
+                    MovieId = movieId
+                }
+            );
+
+            await _context.SaveChangesAsync();
+
+        }
+
+        private async Task AddToMovieCollection(int movieId, int collectionId)
+        {
+            _context.Add(
+                new MovieCollection()
+                {
+                    CollectionId = collectionId,
+                    MovieId = movieId
+                }
+            );
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
